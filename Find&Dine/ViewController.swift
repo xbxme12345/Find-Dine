46e03f91560e5c1771b6f171b1412cd4766b071f
Yan Wen Huang <yanwenhuang.95@gmail.com>
2018-06-03 00:12:50 -0400

    Initial Commit

---
+++ Find&Dine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find&Dine
+//
+//  Created by Yan Wen Huang on 6/3/18.
+//  Copyright © 2018 Wentworth Senior Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
